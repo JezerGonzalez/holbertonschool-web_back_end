74ae10c070c3b5cc1e29d3f48bbb92b5cf95eb6a
JezerGonzalez <jezergonzalez@gmail.com>
2024-07-18 15:29:03 -0400

    task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+export default class Pricing {
+  /**
+     *
+     * @param {Number} ammount
+     * @param {Currency} currency
+     */
+  constructor(ammount, currency) {
+    this._ammount = ammount;
+    this._currency = currency;
+  }
+
+  get ammount() {
+    return this._ammount;
+  }
+
+  set ammount(ammount) {
+    this._ammount = ammount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._ammount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  convertPrice(ammount, conversionRate) {
+    return ammount * conversionRate;
+  }
+}
