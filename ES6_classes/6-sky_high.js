5450b667bff5593e9c206a37b2883087c6a47102
JezerGonzalez <jezergonzalez@gmail.com>
2024-07-18 16:08:21 -0400

    attempt 3

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,17 +3,18 @@ import Building from './5-building'
 export default class SkyHighBuilding extends Building {
     constructor(sqft, floors){
         super(sqft);
-        this.floors = floors;
-        if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
+        this._floors = floors;
     }
 
     get sqft(){
-        return this.sqft;
+        return this._sqft;
     }
 
     get floors(){
-        return this.floors;
+        return this._floors;
+    }
+
+    evacuationWarningMessage(){
+        return 'Evacuate slowly the ${this._floors} floors';
     }
 }
