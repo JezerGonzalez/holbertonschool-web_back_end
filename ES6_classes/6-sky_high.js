d531fbaec04dd7477bd9b6d6fdb53d26e8bfffa1
JezerGonzalez <jezergonzalez@gmail.com>
2024-07-18 16:04:39 -0400

    task 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building'
+
+export default class SkyHighBuilding extends Building {
+    constructor(sqft, floors){
+        super(sqft);
+        this.floors = floors;
+        if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+    }
+
+    get sqft(){
+        return super.sqft;
+    }
+
+    get floors(){
+        return this.floors;
+    }
+
+
+}
