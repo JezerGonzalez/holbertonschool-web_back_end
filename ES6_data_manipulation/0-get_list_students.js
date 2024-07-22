fd1773e51083e2b36a692f28e6eef78120aa5813
JezerGonzalez <jezergonzalez@gmail.com>
2024-07-22 11:47:29 -0400

    task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+  const students = [];
+
+  students.push({ id: 1, firstname: 'Guillaume', location: 'San Francisco' });
+  students.push({ id: 2, firstname: 'James', location: 'Columbia' });
+  students.push({ id: 3, firstname: 'Serena', location: 'San Francisco' });
+
+  return students;
+}
