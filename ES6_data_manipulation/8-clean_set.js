eb3f6870b0582cc1ac1f2b03d47dc9c36f860bef
JezerGonzalez <jezergonzalez@gmail.com>
2024-07-23 12:16:45 -0400

    with if

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
   return Array.from(set)
     .filter((item) => item.startsWith(startString))
     .map((item) => item.slice(startString.length))
