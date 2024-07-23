d517fa4d56edecc95a198c3cc432aef19b53ad58
JezerGonzalez <jezergonzalez@gmail.com>
2024-07-23 12:40:18 -0400

    ya

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -4,7 +4,7 @@ export default function groceriesList() {
   groceries.set('Apples', 10);
   groceries.set('Tomatoes', 10);
   groceries.set('Pasta', 1);
-  groceries.set('Rice', 1 );
+  groceries.set('Rice', 1);
   groceries.set('Banana', 5);
 
   return groceries;
