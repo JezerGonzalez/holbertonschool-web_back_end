21e1a64232abd7e12e088b3b8019adf01e6b2306
JezerGonzalez <jezergonzalez@gmail.com>
2024-07-24 14:34:29 -0400

    reject

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     if (success === true) {
       resolve({
         status: 200,
         body: 'Success',
       });
     } else {
-      throw Error({
+      reject({
         status: 500,
         body: 'The fake API is not working currently',
       });
