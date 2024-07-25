0350bd578a1d3849ea1b157072c8faa12b946715
JezerGonzalez <jezergonzalez@gmail.com>
2024-07-25 13:50:02 -0400

    complete change

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,19 +2,20 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.forEach((result) => {
-    if (result.status === 'fulfilled') {
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value,
+        };
+      }
       return {
-        status: 'fulfilled',
-        value: result.value,
+        status: result.status,
+        value: result.reason,
       };
-    }
-    return [{
-      status: 'rejected',
-      value: result.reason,
-    }];
-  }));
+    }));
 }
