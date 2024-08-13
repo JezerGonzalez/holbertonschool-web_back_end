047be34c01c98c388a9c77eaf8275b4e68475523
JezerGonzalez <jezergonzalez@gmail.com>
2024-08-13 12:47:23 -0400

    task 8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Returns a float multiplied"""
+from typing import Callable
+
+
+def make_mutliplier(multiplier: float) -> Callable[[float], float]:
+    return make_mutliplier(multiplier)
