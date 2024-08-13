a53d4f77e5e8f730cba504f5edb358bc586f6a10
JezerGonzalez <jezergonzalez@gmail.com>
2024-08-13 12:53:32 -0400

    doc

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Yes"""
     def multiplier_funtion(value: float) -> float:
+        """Absolutely"""
         return value * multiplier
     return multiplier_funtion
