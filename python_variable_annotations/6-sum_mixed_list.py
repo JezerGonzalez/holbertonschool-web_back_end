e372c6156d3578e5a83fa4c2e6475cb74e283a8e
JezerGonzalez <jezergonzalez@gmail.com>
2024-08-13 12:08:07 -0400

    lst not list

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """returns the sum of the list"""
-    return sum(mxd_list)
+    return sum(mxd_lst)
