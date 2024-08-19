986a01ce4e0bd673e16c2735fedd6615a971c42c
JezerGonzalez <jezergonzalez@gmail.com>
2024-08-19 15:43:55 -0400

    DOCUMENTATION

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,6 +6,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """Tu mai por si acaso"""
     start_time = time.perf_counter()
     await asyncio.gather(async_comprehension())
     end_time = time.perf_counter()
