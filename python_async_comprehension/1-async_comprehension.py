73e9050a4abb25f678b299f67d12ecd40d3a4052
JezerGonzalez <jezergonzalez@gmail.com>
2024-08-19 14:48:33 -0400

    list typing

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """Frankly, my dear, I don't give a damn"""
     return [i async for i in async_generator()]
