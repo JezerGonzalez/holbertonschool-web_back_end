89d92c6c62ccfd9a589739907e08888a4a7e4662
JezerGonzalez <jezergonzalez@gmail.com>
2024-08-19 13:54:30 -0400

    qsy

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Async Generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Give me liberty, give me death"""
     for number in range(10):
         await asyncio.sleep(1)
