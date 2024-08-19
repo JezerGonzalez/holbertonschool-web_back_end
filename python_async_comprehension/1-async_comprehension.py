52f30f67d50154ed557ee11dfee036cef6a3f8cf
JezerGonzalez <jezergonzalez@gmail.com>
2024-08-19 14:26:30 -0400

    generator

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
+from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> Generator[float, None, None]:
     """Frankly, my dear, I don't give a damn"""
     return [i async for i in async_generator()]
