8aece7176d314995378373de87e874d331270665
JezerGonzalez <jezergonzalez@gmail.com>
2024-08-20 11:00:18 -0400

    * page_size

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,6 +5,6 @@ from typing import Tuple
 
 def index_range(page: int, page_size:int) -> Tuple:
     """Simple helper function"""
-    start = page - 1
+    start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
