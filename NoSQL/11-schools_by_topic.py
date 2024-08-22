5c70c869aeb6015e7ac448fda2c76f81fbd1d2e1
JezerGonzalez <jezergonzalez@gmail.com>
2024-08-22 18:28:23 -0400

    attempt 2

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,5 @@
 
 def schoolsby_topic(mongo_collection, topic):
     """Returns list of school having a specific topic"""
-    mongo_collection.list({"topic": topic})
+    docs = mongo_collection.find({"topics": topic})
+    return docs
