c134a635e3884c52f1c4466b3d58057348eebd96
JezerGonzalez <jezergonzalez@gmail.com>
2024-08-22 18:29:17 -0400

    attempt 3

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -2,7 +2,7 @@
 """Where can I learn Python?"""
 
 
-def schoolsby_topic(mongo_collection, topic):
+def schools_by_topic(mongo_collection, topic):
     """Returns list of school having a specific topic"""
     docs = mongo_collection.find({"topics": topic})
     return docs
