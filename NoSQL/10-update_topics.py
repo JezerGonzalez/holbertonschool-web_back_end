e6ce1d194710c019685938b771c3f7f8e0c87a81
JezerGonzalez <jezergonzalez@gmail.com>
2024-08-22 18:18:44 -0400

    idk

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,5 +4,5 @@
 
 def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on name"""
-    document = mongo_collection.update_many({"name": name, "$set": {"topics": topics}})
+    document = mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
     return document.matched_count, document.modified_count
